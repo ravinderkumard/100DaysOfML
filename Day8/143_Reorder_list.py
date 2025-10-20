a505c6674f2830eca0e1bf665f79395effab1588
Ravinder Kumar <ravinderkumar@Ravinders-Mac-mini.local>
2025-10-20 06:53:28 +0530

    Leetcode program

---
+++ Day8/143_Reorder_list.py
@@ -0,0 +1,73 @@
+"""
+    You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+"""
+
+"""
+    Find the middle of the list
+    Try to break the list into 2 part
+    reverse the second part
+    using 2 pointer for each list attach them with other list.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
+        second = self.reverse(second)
+        first = head
+        while first and second:
+            firstNext = first.next
+            secondNext = second.next
+            first.next = second
+            second.next = firstNext
+            first = firstNext
+            second = secondNext
+        
+    def reverse(self,head):
+        curr = head
+        prev = None
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
